049daf60e37c3a56de5a2b2d2374772ea72c30cb
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2021-12-19 11:52:09 +0000

    Add bootstrap template

--- club/views.py
+++ club/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Event
 
-# Create your views here.
+class EventList(generic.ListView):
+    #view to render the Event objects, max 9 per page
+    model = Event
+
+    Event.objects.all()
+    template_name = 'events.html'
+    paginate_by = 9
