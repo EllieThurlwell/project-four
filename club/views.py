f53b3e030f74d2ec7ce58f34498cb2d3d26a920b
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2021-12-20 14:08:24 +0000

    Install crispy forms and update contact view

--- club/views.py
+++ club/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic, View
 from django.views.generic.base import TemplateView
 from .models import Event
+from .forms import ContactForm
 
 class EventList(generic.ListView):
     #view to render the Event objects, max 9 per page
@@ -12,10 +13,32 @@ class EventList(generic.ListView):
 
 
 class HomePage(TemplateView):
-    
+    #view to render the homepage
     template_name = 'index.html'
 
 
 class Contact(TemplateView):
-    
+    #view to render the Contact form to contact.html
+
+    # forms displays if send clicked. need a get method first?
+    def post(self, request):
+
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.instance.fname = request.user.fname
+            contact_form.instance.lname = request.user.lname
+            contact_form.instance.email = request.user.email
+            message = contact_form.save(commit=False)
+            message.save()
+        else:
+            contact_form = ContactForm()
+
+        return render(
+            request,
+            'contact.html',
+            {
+                'contact_form': contact_form
+            },
+        )
+
     template_name = 'contact.html'
