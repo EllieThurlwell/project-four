c64cb948e7e66270b85854794a5a069d3095088a
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2021-12-22 18:43:44 +0000

    Fix error in contact view to display form

--- club/views.py
+++ club/views.py
@@ -3,7 +3,7 @@ from django.views import generic, View
 from django.views.generic.base import TemplateView
 from django.conf import settings
 from .models import Event
-from .forms import ContactForm
+from .forms import ContactForm, BookingForm
 
 class EventList(generic.ListView):
     #view to render the Event objects, max 9 per page
@@ -15,13 +15,30 @@ class EventList(generic.ListView):
 
 def getMap(request):
 
-    context = { 'api_key': settings.MAPS_API_KEY}
-    return render('events.html', context)
+    context = Context({"api_key": settings.MAPS_API_KEY})
+    # context = { 'api_key': settings.MAPS_API_KEY}
+    return render(request,'events.html', context)
 
 
 class HomePage(TemplateView):
     #view to render the homepage
     template_name = 'index.html'
+
+
+class BookingPage(TemplateView):
+    #view to render the booking page
+
+    def booking(request):
+        if request.method == 'POST':
+            booking_form = BookingForm(data=request.POST)
+        if booking_form.is_valid():
+            booking_form = booking_form.save()
+        else:
+            booking_form = BookingForm()
+        return render(request, 'booking.html', {'booking_form': booking_form})
+
+
+    template_name = 'booking.html'
 
 
 def contact(request):
@@ -30,8 +47,8 @@ def contact(request):
     # forms displays if send clicked. need a get method first?
     if request.method == 'POST':    
         contact_form = ContactForm(data=request.POST)
-    if contact_form.is_valid():
-        contact = contact_form.save()
+        if contact_form.is_valid():
+            contact = contact_form.save()
     else:
         contact_form = ContactForm()
     return render(request, 'contact.html', {'contact_form': contact_form})
